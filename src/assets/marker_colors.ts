a51eb914e0682855263aadfc3ad95b4ef91af017
John Arban Lewis <johnarban@users.noreply.github.com>
2026-09-22 14:07:16 -0400

    update colors - really just make empty more muted

--- src/assets/marker_colors.ts
+++ src/assets/marker_colors.ts
@@ -1,15 +1,20 @@
 // https://davidmathlogic.com/colorblind
-export const paulTolMutedColorblindSafeColors = ['#CC6677', '#332288', '#117733', '#882255', '#44aa99', '#999933', '#aa4499',];
-export const wongColorblindSafeColors = ['#009E73', '#56B4E9', '#CC79A7', '#D55E00','#F0E442', '#0072B2', '#E69F00'];
+// export const paulTolMutedColorblindSafeColors = ['#CC6677', '#332288', '#117733', '#882255', '#44aa99', '#999933', '#aa4499',];
+// export const wongColorblindSafeColors = ['#009E73', '#56B4E9', '#CC79A7', '#D55E00','#F0E442', '#0072B2', '#E69F00'];
+
+// export const markerColors = [
+//   '#00C8DC','#FFC61A','#2FD44F','#9A5CFF','#10121A',
+//   ...wongColorblindSafeColors,
+//   ...paulTolMutedColorblindSafeColors,
+//   '#e6194b', '#3cb44b', '#4363d8', '#911eb4', '#46f0f0',
+//   '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8',
+//   '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff',
+//   '#000000'
+// ];
 
 export const markerColors = [
-  '#00C8DC','#FFC61A','#2FD44F','#9A5CFF','#10121A',
-  ...wongColorblindSafeColors,
-  ...paulTolMutedColorblindSafeColors,
-  '#e6194b', '#3cb44b', '#4363d8', '#911eb4', '#46f0f0',
-  '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8',
-  '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff',
-  '#000000'
+  // empty, isolate, simple, rich
+  '#6d8c8f','#FFC61A','#2FD44F','#9A5CFF','#10121A',
 ];
 
 export const checkmarkColors = [
